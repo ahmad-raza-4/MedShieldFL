04d146966974c9506cd78b0073d07872994c216a
ahmad-thewhiz <inboxofahmad@gmail.com>
2025-01-17 23:36:07 -0800

    dynamic noise clients
    old version with variable epsilon

---
+++ dynamic_noise_clients_old/new_dynamic_noise_clients_1.0/server.py
@@ -0,0 +1,32 @@
+import flwr as fl
+from time import time
+
+start_time = time()
+
+strategy = fl.server.strategy.FedAvg(
+    fraction_fit=1.0,
+    fraction_evaluate=1.0,
+    min_fit_clients=6,  
+    min_evaluate_clients=6,
+    min_available_clients=6,
+)
+
+fl.server.start_server(
+    server_address="0.0.0.0:8075",
+    config=fl.server.ServerConfig(num_rounds=30),
+    strategy=strategy,
+)
+
+end_time = time()
+print(f"Time taken (hours): {(end_time - start_time) / 3600}")
+
+
+try:
+    def save_str_to_file(string, dir: str):
+        with open(f"{dir}/log_file.txt", "a") as file:
+            file.write(string + '\n')
+
+    string = f"Time taken (hours): {(end_time - start_time) / 3600}"
+    save_str_to_file(string, "server")
+except Exception as e:
+    print(f"Failed to save time taken: {e}")
