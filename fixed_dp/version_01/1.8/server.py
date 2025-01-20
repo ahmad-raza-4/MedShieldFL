7ccfdcea7bfe30fedd17089b8fa1656286761ced
ahmad-thewhiz <inboxofahmad@gmail.com>
2025-01-20 03:36:16 -0800

    rearrange

--- fixed_dp/version_01/1.8/server.py
+++ fixed_dp/version_01/1.8/server.py
@@ -14,7 +14,7 @@ strategy = fl.server.strategy.FedAvg(
 # dp_strategy = DifferentialPrivacyClientSideFixedClipping(strategy=strategy, num_sampled_clients=6, noise_multiplier=1.0, clipping_norm=0.5)
 
 fl.server.start_server(
-    server_address="0.0.0.0:8087",
+    server_address="0.0.0.0:8084",
     config=fl.server.ServerConfig(num_rounds=30),
     strategy=strategy,
 )
