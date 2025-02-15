1c423c8fba9eef9bc7449040f5026337bf2b9a4c
ahmad-thewhiz <inboxofahmad@gmail.com>
2025-02-15 12:03:00 -0800

    so many new exp

---
+++ alzheimers/adagrad/fixed/0.43. e=30/server.py
@@ -0,0 +1,37 @@
+import flwr as fl
+import torch
+from main import ViT_GPU
+import os
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = ViT_GPU(device=device).to(device)
+model.eval()
+
+
+def get_parameters(model):
+    """Extract and return model parameters as a list of NumPy arrays."""
+    return [val.cpu().numpy() for _, val in model.state_dict().items()]
+
+initial_parameters = fl.common.ndarrays_to_parameters(get_parameters(model))
+
+
+strategy = fl.server.strategy.FedAdagrad(
+    fraction_fit=1.0,
+    fraction_evaluate=1.0,
+    min_fit_clients=6,
+    min_evaluate_clients=6,
+    min_available_clients=6,
+    initial_parameters=initial_parameters,
+    eta  = 1e-3,
+    eta_l  = 1e-3,
+    tau  = 1e-9, 
+)
+
+
+fl.server.start_server(
+    server_address="0.0.0.0:8023",
+    config=fl.server.ServerConfig(num_rounds=30),
+    strategy=strategy,
+)
+
